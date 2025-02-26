08c8bba29e6eeeb1737a7accb9c2fef6d659c5f1
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-02-26 20:06:48 +0300

    Created 'D - Socialism' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 26-Feb-2025/D - Socialism 285227.py
@@ -0,0 +1,18 @@
+# Problem: D - Socialism - https://codeforces.com/gym/589822/problem/D
+
+t = int(input())
+for _ in range(t):
+    n, x = map(int, input().split())
+    savings = list(map(int, input().split()))
+    
+    savings.sort() 
+    total = sum(savings) 
+    wealthy = 0  
+    for i in range(n):
+        remaining = n - i  
+        if total / remaining >= x:
+            wealthy = remaining
+            break
+        total -= savings[i]  
+    
+    print(wealthy)
