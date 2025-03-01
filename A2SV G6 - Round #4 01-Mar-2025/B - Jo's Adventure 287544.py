37c304924d701c86f5d3a9fadd5dd76086093579
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-01 12:08:24 +0300

    Created 'B - Jo's Adventure' by A2SV Hub

---
+++ A2SV G6 - Round #4 01-Mar-2025/B - Jo's Adventure 287544.py
@@ -0,0 +1,28 @@
+# Problem: B - Jo's Adventure - https://codeforces.com/gym/590053/problem/B
+
+n, m = map(int,input().split())
+building = list(map(int,input().split()))
+forsum = [0] * n
+forsum[0] = 0
+backsum = [0] * n
+backsum[n-1] = 0
+for i in range(1,n):
+    if building[i] < building[i-1]:
+        curr  = building[i-1] - building[i]
+    else:
+        curr = 0
+    forsum[i] = curr + forsum[i-1]
+for i in range(n-2,-1,-1):
+    if building[i] < building[i+1]:
+        curr  = building[i+1] - building[i]
+    else:
+        curr = 0
+    backsum[i] = curr + backsum[i+1]
+for _ in range(m):
+    l, r = list(map(int,input().split()))
+    if l > r:
+        ans = backsum[r-1] - backsum[l-1] 
+        print(ans)
+    else:
+        ans = forsum[r-1] - forsum[l-1]
+        print(ans)
