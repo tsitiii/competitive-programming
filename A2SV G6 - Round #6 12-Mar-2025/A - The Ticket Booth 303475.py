e2550df4da818bbfb408f180fe20903ef2b9775e
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-12 04:27:27 +0300

    Created 'A - The Ticket Booth' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/A - The Ticket Booth 303475.py
@@ -0,0 +1,9 @@
+# Problem: A - The Ticket Booth - https://codeforces.com/gym/594077/problem/A
+
+
+n, target = map(int, input().split())
+ans = target // n
+if target % n != 0:
+    ans +=1
+print(ans)
+
