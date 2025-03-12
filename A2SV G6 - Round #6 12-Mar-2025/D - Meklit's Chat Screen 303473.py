01b5577c59ff37f2311c710c596046b9bae1ae34
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-12 04:26:49 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/D - Meklit's Chat Screen 303473.py
@@ -0,0 +1,25 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import deque
+n ,  k = map(int,input().split())
+message = list(map(int,input().split()))
+dq = deque()
+seen = set()
+for i in range(len(message)):
+    if not dq:
+        dq.append(message[i])
+        seen.add(message[i])
+    if message[i] in seen:
+        continue
+    elif len(dq) >= k:
+        rm = dq.popleft()
+        seen.remove(rm)
+        dq.append(message[i])
+        seen.add(message[i])
+    else:
+        dq.append(message[i])
+        seen.add(message[i])
+    
+print(len(dq))
+for i in range(len(dq)-1, -1, -1):
+    print(dq[i],end=' ')
