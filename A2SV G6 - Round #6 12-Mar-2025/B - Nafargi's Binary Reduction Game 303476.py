403da025fefd5030335343c8d2aba7cd77e3d63c
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-12 04:27:42 +0300

    Created 'B - Nafargi's Binary Reduction Game' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/B - Nafargi's Binary Reduction Game 303476.py
@@ -0,0 +1,11 @@
+# Problem: B - Nafargi's Binary Reduction Game - https://codeforces.com/gym/594077/problem/B
+
+n = int(input())
+string =  input()
+stack = []
+for  char in string:
+    if stack and char != stack[-1]:
+        stack.pop()
+    else:
+        stack.append(char)
+print(len(stack))
