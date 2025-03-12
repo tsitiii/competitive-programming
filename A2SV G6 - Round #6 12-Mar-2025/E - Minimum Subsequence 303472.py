e24a9d2c6bf674bad4cdab876db2b61b29229411
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-12 04:26:40 +0300

    Created 'E - Minimum Subsequence' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/E - Minimum Subsequence 303472.py
@@ -0,0 +1,28 @@
+# Problem: E - Minimum Subsequence - https://codeforces.com/gym/594077/problem/E
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    string = input()
+    zeros = []
+    ones = []
+    group = [0] * n
+    cnt = 0
+    for i in range(n):
+        char = string[i]
+        if char == '0':
+            if ones:
+                group[i] = ones.pop()
+            else:
+                cnt += 1
+                group[i] = cnt
+            zeros.append(group[i])
+        else:
+            if zeros:
+                group[i] = zeros.pop()
+            else:
+                cnt += 1
+                group[i] = cnt
+            ones.append(group[i])
+    print(cnt)
+    print(' '.join(map(str, group)))
