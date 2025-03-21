7b2d8923a344d9bf599b4e96c449aa964a577091
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-21 20:14:33 +0300

    Created 'A - Free Ice Cream for the Kids' by A2SV Hub

---
+++ A2SV G6 - Round #7 21-Mar-2025/A - Free Ice Cream for the Kids 315065.py
@@ -0,0 +1,22 @@
+# Problem: A - Free Ice Cream for the Kids - https://codeforces.com/gym/596141/problem/A
+
+from collections import deque
+n,x = map(int, input().split())
+dq = deque()
+children =[]
+distress = 0
+for _ in range(n):
+    string = input()
+    children.append(string)
+for char  in children:
+    if '+' in char:
+        a,b = char.split()
+        x += int(b)
+    elif '-' in char:
+        a,b = char.split()
+        if x >= int(b):
+            x-= int(b) 
+        else:
+            distress += 1
+print(x, distress)
+
