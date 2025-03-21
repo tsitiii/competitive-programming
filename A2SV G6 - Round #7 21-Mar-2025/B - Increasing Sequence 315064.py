86db5f54389dd61ecfde0719e89923db27d30da3
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-21 20:14:24 +0300

    Created 'B - Increasing Sequence' by A2SV Hub

---
+++ A2SV G6 - Round #7 21-Mar-2025/B - Increasing Sequence 315064.py
@@ -0,0 +1,20 @@
+# Problem: B - Increasing Sequence - https://codeforces.com/gym/596141/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = []
+    for i in range(n):
+        if i == 0:
+            if a[i] == 1:
+                b.append(2)
+            else:
+                b.append(1)
+        else:
+            ans = b[i-1] + 1
+            if ans == a[i]:
+                ans += 1
+            b.append(ans)
+    
+    print(b[-1])
