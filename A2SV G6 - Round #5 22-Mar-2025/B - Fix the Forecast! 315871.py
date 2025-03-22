52915e696e93a83ccd6e58238f3018541df7df0d
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-22 23:39:53 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 22-Mar-2025/B - Fix the Forecast! 315871.py
@@ -0,0 +1,21 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+import sys
+
+def solve():
+    t = int(sys.stdin.readline().strip())
+
+    for _ in range(t):
+        n, k = map(int, sys.stdin.readline().split())
+        a = list(map(int, sys.stdin.readline().split()))
+        b = list(map(int, sys.stdin.readline().split()))
+
+        a_with_index = sorted((a[i], i) for i in range(n))  # Sort by values of `a`
+        b.sort() 
+        ans = [0] * n
+        for i in range(n):
+            _, ind = a_with_index[i]  
+            ans[ind] = b[i]  
+        
+        print(*ans)
+solve()
