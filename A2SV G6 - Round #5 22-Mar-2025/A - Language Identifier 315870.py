a2383f385220dbe4f541415ad07b8850e687219a
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-22 23:39:49 +0300

    Created 'A - Language Identifier' by A2SV Hub

---
+++ A2SV G6 - Round #5 22-Mar-2025/A - Language Identifier 315870.py
@@ -0,0 +1,11 @@
+# Problem: A - Language Identifier - https://codeforces.com/gym/591928/problem/A
+
+t = int(input())
+for _ in range(t):
+    s = input()
+    if s[-1] == 'o':
+        print("FILIPINO")
+    elif s[-1] == 'u':
+        print("JAPANESE")
+    elif s[-1] == 'a':
+        print( "KOREAN")
