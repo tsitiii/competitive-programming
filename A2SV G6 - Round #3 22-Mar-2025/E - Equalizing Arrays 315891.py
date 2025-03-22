f3a888a6943c72744f5dd95266f5dc298a53cc76
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-22 23:54:54 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 22-Mar-2025/E - Equalizing Arrays 315891.py
@@ -0,0 +1,29 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+n = int(input())
+arr1 = list(map(int, input().split()))
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+left ,right = 0, 0
+length = 0
+sum1 = sum2 = 0
+while left < n and right < m:
+    sum1 += arr1[left]
+    sum2 += arr2[right]
+    
+    if sum1 == sum2:
+        length += 1
+        left += 1
+        right += 1
+    elif sum1 < sum2:
+        sum2 -= arr2[right]
+        left += 1
+    else:
+        sum1 -= arr1[left]
+        right += 1
+        
+if sum1 == sum2  and left == n and right == m:
+    print(length)
+else:
+    print(-1)
