449cb55b99c6a38094df4a25b7bd59c67edd02fa
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-23 23:40:11 +0300

    Created 'Generate Parentheses' by A2SV Hub

---
+++ 23-Mar-2025/Generate Parentheses 316392.py
@@ -0,0 +1,18 @@
+# Problem: Generate Parentheses - https://leetcode.com/problems/generate-parentheses/description/
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        result = []
+        self.backtrack(result, "", 0, 0, n)
+        return result
+
+    def backtrack(self, result, current, open_count, close_count, n):
+        if len(current) == 2*n:
+            result.append(current) 
+            return
+        if open_count < n:
+            self.backtrack(result, current + "(", open_count + 1, close_count,n)
+        if close_count < open_count:
+            self.backtrack(result, current + ")", open_count, close_count + 1, n)
+
+        
