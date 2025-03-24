22686b8e32cae48b7c9e308c096d87ecb6d56a58
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-03-24 15:11:22 +0300

    Created 'C - Escape-Proof Transfers' by A2SV Hub

---
+++ A2SV G6 - Round #5 24-Mar-2025/C - Escape-Proof Transfers 316645.py
@@ -0,0 +1,16 @@
+# Problem: C - Escape-Proof Transfers - https://codeforces.com/gym/591928/problem/C
+
+n, t, c = [int(i) for i in input().split()]
+prisoners = [int(i) for i in input().split()]
+
+answer = 0
+left = 0
+for right in range(n):
+    if prisoners[right] > t:
+        left = right + 1 
+    
+    current_length = right - left + 1
+    if  current_length >= c:
+        answer += 1
+
+print(answer)
