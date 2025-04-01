2511c2b162f7e3d49f763f2b4adfefef17059c01
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-04-01 20:03:21 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 01-Apr-2025/D - Minimize the Distance 323470.py
@@ -0,0 +1,9 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n = int(input())
+coor = list(map(int, input().split()))
+coor.sort()
+if n % 2 == 0:
+    print(coor[ (n//2) -1])
+else:
+    print(coor[ ((n+1) // 2) - 1])
