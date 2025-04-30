e1dadce6eb47c62083ef3e0a756a5a7694b85010
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-04-30 14:32:21 +0300

    Created 'Minimum Operations to Exceed Threshold Value II' by A2SV Hub

---
+++ 30-Apr-2025/Minimum Operations to Exceed Threshold Value II 345342.py
@@ -0,0 +1,21 @@
+# Problem: Minimum Operations to Exceed Threshold Value II - https://leetcode.com/problems/minimum-operations-to-exceed-threshold-value-ii/
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.min_heap =[num for num in nums]
+        heapq.heapify(self.min_heap)
+        
+    def add(self, val: int) -> int:
+        heapq.heappush(self.min_heap, val)
+        while len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+        return self.min_heap[0]
+
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
