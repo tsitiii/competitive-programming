02f9845f94607bd876042078babbebc2e1d58a2f
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-05-14 20:09:58 +0300

    Created 'A - Can I Get Your Number ?' by A2SV Hub

---
+++ A2SV G6 - Round #13 14-May-2025/A - Can I Get Your Number ? 355619.py
@@ -0,0 +1,20 @@
+# Problem: A - Can I Get Your Number ? - https://codeforces.com/gym/607625/problem/A
+
+from collections import defaultdict
+n = int(input())
+string = []
+for _ in range(n):
+    phone = input()
+    num = list(phone)
+    string.append(phone)
+
+ans = string[0]
+
+for i in range(1,n):
+    k=0
+    temp =''
+    while k <len(ans) and ans[k]==string[i][k]:
+        temp += ans[k]
+        k += 1
+    ans = temp
+print(len(ans))
