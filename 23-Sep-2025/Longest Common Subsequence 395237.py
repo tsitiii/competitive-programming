dd3c437f7ba5d8878af446e249f347332d81f7ce
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-09-23 15:25:07 +0300

    Created 'Longest Common Subsequence' by A2SV Hub

---
+++ 23-Sep-2025/Longest Common Subsequence 395237.py
@@ -0,0 +1,17 @@
+# Problem: Longest Common Subsequence - https://leetcode.com/problems/longest-common-subsequence/
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = {}
+        def dp(i, j):
+            if i >= len(text1) or j >= len(text2):
+                return 0
+            state = (i, j)
+            if state not in memo:
+                if text1[i] == text2[j]:
+                    memo[state] = 1 + dp(i + 1, j +1)
+                else:
+                    memo[state] = max(dp(i + 1, j), dp(i, j+1))
+            return memo[state]
+        res = dp(0, 0)
+        return res
