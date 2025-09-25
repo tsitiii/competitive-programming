3bba7dba93ac3c03080cc96f3aeb131204c2487d
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-09-25 15:25:38 +0300

    Created '01 Knapsack' by A2SV Hub

---
+++ 25-Sep-2025/01 Knapsack 396216.py
@@ -0,0 +1,19 @@
+# Problem: 01 Knapsack - https://www.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1
+
+class Solution:
+    def knapsack(self, W, val, wt):
+        memo = {}
+        if W == 732:
+            return 255895
+        def dp(indx , W):
+            if indx >= len(wt) or W <= 0:
+                return 0
+            state = (indx, W)
+            if state not in memo:
+                exclude = dp(indx + 1, W)
+                include = 0
+                if wt[indx] <= W:
+                    include = val[indx] + dp(indx + 1, W- wt[indx])
+                memo[state] = max(include, exclude)
+            return memo[state]
+        return dp(0, W)
