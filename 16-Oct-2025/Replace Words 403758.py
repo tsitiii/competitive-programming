6a83756b6060764f656c2197e24c8a7ea5c53b6b
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-10-16 19:32:47 +0300

    Created 'Replace Words' by A2SV Hub

---
+++ 16-Oct-2025/Replace Words 403758.py
@@ -0,0 +1,12 @@
+# Problem: Replace Words - https://leetcode.com/problems/replace-words/
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        dictionary.sort(key=len)       
+        words = sentence.split() 
+        for i, word in enumerate(words):
+            for root in dictionary:
+                if word.startswith(root):
+                    words[i] = root
+                    break  
+        return " ".join(words)
