697e9236a961db75f6e6b11bd08d0022070c51da
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2025-11-13 10:00:46 +0300

    Created 'Same Tree' by A2SV Hub

---
+++ 13-Nov-2025/Same Tree 408718.py
@@ -0,0 +1,11 @@
+# Problem: Same Tree - https://leetcode.com/problems/same-tree/description/
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
