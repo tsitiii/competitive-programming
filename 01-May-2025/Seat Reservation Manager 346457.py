6de8df14646db247117884134cd1e1b3d54f2312
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2026-01-28 23:15:05 +0300

    Update Seat Reservation Manager 346457.py

--- 01-May-2025/Seat Reservation Manager 346457.py
+++ 01-May-2025/Seat Reservation Manager 346457.py
@@ -14,6 +14,6 @@ class SeatManager:
 
 
 # Your SeatManager object will be instantiated and called as such:
-# obj = SeatManager(n)
+# objjj = SeatManager(n)
 # param_1 = obj.reserve()
 # obj.unreserve(seatNumber)
