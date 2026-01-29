bbc4ca4dbfd7ea14288038b3d7fe5db5126d5ed9
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2026-01-29 23:34:26 +0300

    Update Search a 2D Matrix 326468.py

--- 04-Apr-2025/Search a 2D Matrix 326468.py
+++ 04-Apr-2025/Search a 2D Matrix 326468.py
@@ -1,4 +1,4 @@
-# Problem: Search a 2D Matrix - https://leetcode.com/problems/search-a-2d-matrix/
+# Problem: Search a 2D Matrix --- https://leetcode.com/problems/search-a-2d-matrix/
 
 from typing import List
 
