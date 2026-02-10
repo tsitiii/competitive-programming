fa1b1730cb7f5022f4b079530c97e03bb029e542
Tsiyon Gashaw <130053047+tsitiii@users.noreply.github.com>
2026-02-10 22:23:43 +0300

    Update Find Xor-Beauty of Array 355409.py

--- 14-May-2025/Find Xor-Beauty of Array 355409.py
+++ 14-May-2025/Find Xor-Beauty of Array 355409.py
@@ -1,4 +1,4 @@
-# Problem: Find Xor-Beauty of Array - https://leetcode.com/problems/find-xor-beauty-of-array/
+# problem: Find Xor-Beauty of Array - https://leetcode.com/problems/find-xor-beauty-of-array/
 
 class Solution:
     def xorBeauty(self, nums: List[int]) -> int:
